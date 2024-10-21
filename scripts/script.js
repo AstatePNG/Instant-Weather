4bb61c2fb084eeb95218a40e46b1fbab790ffe68
Elsa Hamon <elsa.hamon@etu.unicaen.fr>
2024-10-21 22:21:06 +0200

    add of a slider for the number of day and update of the display accordingly

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
 const months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 const days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
-const classes =  ["card_snow", "card_rain", "card_sun", "card_thunder", "card_cloud", "card_fog"];
+const classes =  ["card-snow", "card-rain", "card-sun", "card-thunder", "card-cloud", "card-fog"];
 
-let zipCode;
-
+let zipCode = document.getElementById("zipCode");
 let affichage = document.getElementById("affichage_communes");
 let maVille = document.getElementById("maVille");
 let duree = document.getElementById("duree");
@@ -16,18 +15,21 @@ let nextDaysContainer = document.getElementById("nextDays");
 let openMenu = document.getElementById("openMenu");
 let validFormMenu = document.getElementById("validFormMenu");
 let card = document.getElementById("card");
+let slider = document.getElementById("duration");
+let sliderValueDisplay = document.getElementById("durationValueDisplay");
 
-function clearCardClasses(){ //Remove all the classes to put another one after
-    card.classList.remove(...classes);
+// Remove all the classes to put another one after
+function clearCardClasses(cardToClear){ 
+    cardToClear.classList.remove(...classes);
 }
 
+// Call of the API to get commune code with ZIP code
 async function searchByZipCode(zipCode) {
     try {
         const reponse = await fetch(
             `https://geo.api.gouv.fr/communes?codePostal=${zipCode}`
         );
         const data = await reponse.json();
-        console.table(data)
 
         affichage.innerHTML = "";
         let option = document.createElement("option");
@@ -36,12 +38,12 @@ async function searchByZipCode(zipCode) {
         affichage.appendChild(option);
 
         if (data.length > 0) {
-            data.forEach((commune) => {
-            option = document.createElement("option");
-            option.value = commune.code;
-            option.textContent = commune.nom;
-            affichage.appendChild(option);
-        });
+                data.forEach((commune) => {
+                option = document.createElement("option");
+                option.value = commune.code;
+                option.textContent = commune.nom;
+                affichage.appendChild(option);
+            });
         }
 
     }
@@ -51,6 +53,7 @@ async function searchByZipCode(zipCode) {
     }
 }
 
+// Call of the Weather API to get the informations
 async function getWeatherInformations(comCode) {
     try {
         const repMeteo = await fetch(
@@ -71,20 +74,9 @@ async function getWeatherInformations(comCode) {
         else{
             dureeSoleil.innerHTML = hours + " heures";
         }
-
-        clearCardClasses();
-        card.classList.add('card');
-        if(dataMeteo.forecast[0].probarain > 50){//If Rain proba > 60% display rain background
-            card.classList.add('card-rain');
-        } else if(dataMeteo.forecast[0].sun_hours >= 5){//We consider it sunny
-            card.classList.add('card-sun');
-        }else if(dataMeteo.forecast[0].probafog > 50){ // Doesn't seems to work for now...
-            card.classList.add('card-fog');
-        } else { //Cloudy
-            card.classList.add('card-cloud');
-        }
-        for(let i=1; i<7; i++){
-            addDayCard(dataMeteo.forecast[i].datetime, dataMeteo.forecast[i].tmin, dataMeteo.forecast[i].tmax, dataMeteo.forecast[i].probarain, dataMeteo.forecast[i].sun_hours);
+        setVisualOfWeather(card, dataMeteo.forecast[0].weather);
+        for(let i=1; i<slider.value; i++){
+            addDayCard(dataMeteo.forecast[i].datetime, dataMeteo.forecast[i].tmin, dataMeteo.forecast[i].tmax, dataMeteo.forecast[i].probarain, dataMeteo.forecast[i].sun_hours, dataMeteo.forecast[i].weather);
         }
     }
     catch (error) {
@@ -93,44 +85,54 @@ async function getWeatherInformations(comCode) {
     }
 }
 
+
+// Call of the Weather API on click
 checkWeather.addEventListener("click", ()=> {
     getWeatherInformations(affichage.value);
     nextDaysContainer.innerHTML = '';
 });
 
+// Open the settings menu on click
 openMenu.addEventListener("click", ()=> {
     document.getElementById('formMenu').style.display = 'flex';
     document.getElementById('information').style.display = 'none';
-
 })
 
+// Valid the settings on click
 validFormMenu.addEventListener("click", ()=> {
     document.getElementById('information').style.display = 'inline';
     document.getElementById('formMenu').style.display = 'none';
-
 })
 
+// Update the display above the slider to make it match the current value set
+slider.addEventListener("input", ()=> {
+    if(slider.value == 1){
+        sliderValueDisplay.innerHTML = "aujourd'hui";
+    }
+    else{
+        sliderValueDisplay.innerHTML = "pour les " + slider.value + " prochains jours";
+    }
+})
+
+// Call the function for the commune code when a valid Zip code is detected
 function formInput() {
-    zipCode = document.getElementById('zipCode').value;
-    if(/^(0[1-9]|[1-8][0-9]|9[0-8]){1}([0-9]){3}$/.test(zipCode)){
-        searchByZipCode(zipCode);  //variable containing a valid postal code
+    let zipCodeVal = zipCode.value;
+    if(/^(0[1-9]|[1-8][0-9]|9[0-8]){1}([0-9]){3}$/.test(zipCodeVal)){
+        searchByZipCode(zipCodeVal);  //variable containing a valid postal code
     }
 }
 
-function addDayCard(date, min, max, proba, sol) {
+// Add a new weather card when called
+function addDayCard(date, min, max, proba, sol, weather) {
     const dateI = new Date(date);
+    let inline = document.createElement("div");
     let meteoDay = document.createElement("div");
-    meteoDay.classList.add("card");
     let meteoBody = document.createElement("div");
-    meteoBody.classList.add("card-body");
     let time = document.createElement("p");
     let probrain = document.createElement("p");
     let suntime = document.createElement("p");
     let minmax = document.createElement("p");
-    time.classList.add("card-text", "d-flex", "flex-row", "justify-content-center", "mb-3");
-    probrain.classList.add("card-text", "d-flex", "flex-row", "justify-content-center");
-    suntime.classList.add("card-text", "d-flex", "flex-row", "justify-content-center");
-    minmax.classList.add("card-text", "d-flex", "flex-row", "justify-content-center");
+
     time.innerHTML = days[dateI.getDay()] + " " + dateI.getDate() + " " + months[dateI.getMonth()] + " " + dateI.getFullYear();
     probrain.innerHTML = "Probabilité de pluie : " + proba + "%";
     if(sol == 1){
@@ -141,11 +143,26 @@ function addDayCard(date, min, max, proba, sol) {
     }
     minmax.innerHTML = min + '°C <span class="fleche"><span class="gauche">⬅</span><span class="droite">➡</span></span>' + max + "°C";
 
-
     meteoBody.appendChild(time);
     meteoBody.appendChild(probrain);
     meteoBody.appendChild(suntime);
     meteoBody.appendChild(minmax);
     meteoDay.appendChild(meteoBody);
-    nextDaysContainer.appendChild(meteoDay);
+    inline.appendChild(meteoDay);
+    nextDaysContainer.appendChild(inline);
+    setVisualOfWeather(meteoDay, weather);
+
+    inline.classList.add("col", "mb-4");
+    meteoDay.classList.add("card", "text-center");
+    meteoBody.classList.add("card-body");
+    time.classList.add("card-text");
+    probrain.classList.add("card-text");
+    suntime.classList.add("card-text");
+    minmax.classList.add("card-text");
+}
+
+// change the displayed image after checking the weather informations
+function setVisualOfWeather(cardToUpdate, weather) {
+    clearCardClasses(cardToUpdate);
+    cardToUpdate.classList.add('card');
 }
